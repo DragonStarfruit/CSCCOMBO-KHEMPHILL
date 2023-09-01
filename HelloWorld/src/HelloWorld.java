ac37213121bfc8c474b7efd66153ffe75537fb29
Hemphill <S02872326@ccc.ccofc.edu>
2023-09-01 16:22:32 -0600

    hello

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+		/*************************************										
+		 * multi-line comment (more Profetional)									*
+		 * this is a hello world program. It writes hello world in the console		*
+		 * written by Kai Hemphill on 9/1/2023										*
+		 *************************************/	
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Hello World comment 
+		System.out.println("Hello World!");		
+		// sysout(ctrl + spacebar) <-- shortcut
+		System.out.println("Hello again!");
+		
+		int num1 = 66; 
+		
+		int num2 = 12;
+		
+		int feet = num1 / num2; 
+		// % does div remainder 
+		int inches = num1 % num2; 
+		
+		System.out.println("The item is " + feet + "feet and " + inches + "in"); 
+
+	}
+
+}
