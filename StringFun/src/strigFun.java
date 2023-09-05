154fc115ebffe4ec1eebdae232e8ead04634b1a6
Hemphill <S02872326@ccc.ccofc.edu>
2023-09-05 15:58:16 -0600

    Fun with Strings and a little input

---
+++ StringFun/src/strigFun.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class strigFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String answer = 
+		
+		String day = "Tuesday"; 
+		
+		int date = 5; 
+		
+		String month = "September";
+		
+		int year = 2023; 
+		
+		System.out.println(day + " " + month + " " + date + ", " + year);
+		// to comment quotes use "/"words"/"
+		System.out.print(day + " ");
+		System.out.print(date + " ");
+		System.out.print(month + " ");
+		System.out.print(year + " ");
+		
+		Scanner input = new Scanner(System.in);  
+		
+		System.out.println("Do you know how to get input?);
+		answer = input.nextLine(); 
+		
+		System.out.println(answer);
+		
+	}
+
+}
