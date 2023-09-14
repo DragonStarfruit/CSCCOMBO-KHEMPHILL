3e8138e1df6d90ace2a1901bd3a13003d1161183
Hemphill <S02872326@ccc.ccofc.edu>
2023-09-14 16:11:52 -0600

    ForLoop

---
+++ ForLoops/src/forLoops.java
@@ -0,0 +1,26 @@
+/************************************
+ * 			For Loops				*
+ ***********************************/
+public class forLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		int sum = 0; 
+		
+//		while (i <= 10) {
+//			System.out.println("i = " + i);
+//			i++; 
+//			sum += + i;
+//		}
+//		System.out.println("The sum of all the numbers is " + sum);
+		
+		for (i = 0; i <= 10; i++) {
+			System.out.println("i = " + i);
+			sum += + i; 
+		}
+		System.out.println("The sum of all the numbers is " + sum);
+	}
+	
+
+}
