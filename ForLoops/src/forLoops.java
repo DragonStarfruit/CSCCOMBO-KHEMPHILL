fd56dec0bc78f5fcd08a79dd1b6dde1015eec785
Hemphill <S02872326@ccc.ccofc.edu>
2023-09-18 16:06:04 -0600

    TemperatureConverterMethod

--- ForLoops/src/forLoops.java
+++ ForLoops/src/forLoops.java
@@ -7,7 +7,7 @@ public class forLoops {
 		// TODO Auto-generated method stub
 		int i = 0;
 		int sum = 0; 
-		
+		int j = 0;
 //		while (i <= 10) {
 //			System.out.println("i = " + i);
 //			i++; 
@@ -15,12 +15,18 @@ public class forLoops {
 //		}
 //		System.out.println("The sum of all the numbers is " + sum);
 		
-		for (i = 0; i <= 10; i++) {
-			System.out.println("i = " + i);
-			sum += + i; 
+//		for (i = 0; i <= 10; i++) {
+//			System.out.println("i = " + i);
+//			sum += + i; 
+//		}
+//		System.out.println("The sum of all the numbers is " + sum);
+		
+		for (i = 0; i <= 5; i++) {
+			for (j = 0; j <= 5; j++) {
+				System.out.print(1);
+			}
 		}
-		System.out.println("The sum of all the numbers is " + sum);
-	}
+			
 	
 
 }
